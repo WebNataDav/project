af282917b00d3bfb56192ad7b0935164b7ef5172
Natalia Davydova <dav.nata@mail.ru>
2025-06-20 23:27:28 +0500

    added HomePage, FavouritesPage, added store and api

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import {Routes, Route} from 'react-router-dom'
 import {HomePage} from './pages/HomePage';
 import {FavouritesPage} from './pages/FavouritesPage';
+import {Navigation} from './components/Navigation';
 import './App.css'
 
 function App() {
 
   return (
+    <>
+    <Navigation/>
     <Routes>
     <Route path="/" element={<HomePage/>}/>
     <Route path="/favourites" element={<FavouritesPage/>}/>
     </Routes>
+    </>
   )
 }
 
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import {useSearchUsersQuery} from '../store/github/github.api'
 
 export function HomePage(){
+  const {isLoading, isError, data} = useSearchUsersQuery('Nata');
+  console.log('data', data)
   return (
     <div>home</div>
   )
