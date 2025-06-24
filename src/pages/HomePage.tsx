748c757af0d8b6fb828f9e327a3755042da8100c
Natalia Davydova <dav.nata@mail.ru>
2025-06-24 21:52:26 +0500

    added RepoCard

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
-import {useSearchUsersQuery} from '../store/github/github.api'
+import {useSearchUsersQuery, useLazyGetUserReposQuery} from '../store/github/github.api'
 import { useDebounce } from '../hooks/debounce';
+import { RepoCard } from '../components/RepoCard';
 
 export function HomePage(){
   const [search, setSearch] = useState('');
@@ -9,13 +10,23 @@ export function HomePage(){
   const debounced = useDebounce(search);
 
   const {isLoading, isError, data} = useSearchUsersQuery(debounced, {
-    skip: debounced.length < 3
+    skip: debounced.length < 3,
+    refetchOnFocus: true
   });
+
+  const [fetchRepos, {isLoading: areReposLoading, data: repos}] = useLazyGetUserReposQuery();
+
+  console.log('repos', repos);
 
   useEffect(() => {
     setDropdown(debounced.length > 3 && data?.length! > 0)
 
   }, [debounced, data]);
+
+  const clickHandler = (userName: string) => {
+    fetchRepos(userName);
+    setDropdown(false);
+  } 
 
   return (
     <div className="flex justify-center pt-10 mx-auto h-screen w-screen">
@@ -33,6 +44,7 @@ export function HomePage(){
           {isLoading && <p className="text-center">Loading...</p>}
           {data?.map(user => (
             <li 
+            onClick={() => clickHandler(user.login)}
             className="py-2 px-2 hover:bg-gray-500 hover:text-white transition-colors cursor-pointer"
             key={user.id}>
               {user.login}
@@ -40,7 +52,12 @@ export function HomePage(){
           )) }
        </ul>
         )}
+        <div className="container">
+        {areReposLoading && <p className="text-center">Repos are loading...</p>}
+        {repos?.map(repo => <RepoCard key={repo.id} repo={repo}/>)}
       </div>
+      </div>
+      
     </div>
   )
 }
--- src/store/github/github.api.ts
+++ src/store/github/github.api.ts
@@ -1,11 +1,12 @@
-import { ServerResponse, IUser } from './../../models/models';
+import { ServerResponse, IUser, IRepo } from './../../models/models';
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 
 export const githubApi = createApi({
   reducerPath: 'github/api',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://api.github.com/'
+    baseUrl: 'https://api.github.com/',
   }),
+  refetchOnFocus: true,
   endpoints: build => ({
     searchUsers: build.query<IUser[], string>({
       query: (search: string) => ({
@@ -16,8 +17,13 @@ export const githubApi = createApi({
         }
       }),
       transformResponse: (response: ServerResponse<IUser>) => response.items
+    }),
+    getUserRepos: build.query<IRepo[], void>({
+      query: (username: string) => ({
+        url: `users/${username}/repos`
+      })
     })
   })
 })
 
-export const {useSearchUsersQuery} = githubApi;
+export const {useSearchUsersQuery, useLazyGetUserReposQuery} = githubApi;
